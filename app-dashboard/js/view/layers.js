e394266a1d30c11d23969a63d5441f912c0e8f76
ann26 <anita@kartoza.com>
2019-12-09 10:17:29 +0700

    remove road and river layers from the map

--- app-dashboard/js/view/layers.js
+++ app-dashboard/js/view/layers.js
@@ -1,23 +1,16 @@
 define([
     'backbone', 'leaflet',
     'js/view/layers/osm-buildings.js',
-    'js/view/layers/osm-rivers.js',
-    'js/view/layers/osm-roads.js',
     'js/view/layers/layer-statistic/osm-polygon-statistic.js'], function (
-    Backbone, L,
-    OSMBuildings, OSMRivers, OSMRoads, PolygonStatistic) {
+    Backbone, L, OSMBuildings, PolygonStatistic) {
     return Backbone.View.extend({
         initialize: function (mapView) {
             let buildings = new OSMBuildings(mapView);
-            let rivers = new OSMRivers(mapView);
-            let roads = new OSMRoads(mapView);
-            this.layers = [buildings, rivers, roads];
+            this.layers = [buildings];
             this.groups = {
                 'Buildings': buildings.group,
-                'Rivers': rivers.group,
-                'Roads': roads.group,
             }
-            this.polygonStatistic = new PolygonStatistic([buildings.statistic, rivers.statistic, roads.statistic]);
+            this.polygonStatistic = new PolygonStatistic([buildings.statistic]);
         },
     });
 });
